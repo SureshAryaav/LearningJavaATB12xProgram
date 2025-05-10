4ba8ad43f33f1f37f7d089d3f6b294e41e4b70ee
SureshAryaav <Suresharyaav@gmail.com>
2025-05-10 20:26:14 +0530

    Comments in Java

---
+++ src/ex_01_JavaBasics/Lab002_comments.java
@@ -0,0 +1,20 @@
+package ex_01_JavaBasics;
+/**
+ * Author : Suresh Ayyanna
+ * Learning :  Java
+ *
+ **/
+public class Lab002_comments {
+    //Main method
+    public static void main(String[] args) {
+        // This is comment, it will be ignored by the compiler
+
+        //Single line comment
+        System.out.println("Single line Comment");
+
+        /*
+        Multi line comment
+         */
+        System.out.println("Multi line Comment");
+    }
+}

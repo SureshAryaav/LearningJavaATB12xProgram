a517ab52d2f7831fb3dfcd5481594df340d5ac51
SureshAryaav <Suresharyaav@gmail.com>
2025-05-10 21:12:30 +0530

    Keywords and Identifier in Java

---
+++ src/ex_01_JavaBasics/Lab006_CommonMathException.java
@@ -0,0 +1,7 @@
+package ex_01_JavaBasics;
+
+public class Lab006_CommonMathException {
+    public static void main(String[] args) {
+        System.out.println(10/0); // "main" java.lang.ArithmeticException: / by zero
+    }
+}

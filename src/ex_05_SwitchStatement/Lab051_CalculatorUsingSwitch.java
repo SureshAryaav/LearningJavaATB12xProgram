f224af69f93493c9654a07cb93157b4678175d02
SureshAryaav <Suresharyaav@gmail.com>
2025-05-18 21:00:46 +0530

    Java notes -01

---
+++ src/ex_05_SwitchStatement/Lab051_CalculatorUsingSwitch.java
@@ -0,0 +1,55 @@
+package ex_05_SwitchStatement;
+
+import java.util.Scanner;
+
+public class Lab051_CalculatorUsingSwitch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input numbers
+        System.out.print("Enter first number: ");
+        double num1 = sc.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double num2 = sc.nextDouble();
+
+        // Display operations
+        System.out.println("Choose operation: +, -, *, /");
+        char operator = sc.next().charAt(0);
+
+        double result;
+
+        // Switch statement based on operator
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                System.out.println("Result: " + result);
+                break;
+
+            case '-':
+                result = num1 - num2;
+                System.out.println("Result: " + result);
+                break;
+
+            case '*':
+                result = num1 * num2;
+                System.out.println("Result: " + result);
+                break;
+
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    System.out.println("Result: " + result);
+                } else {
+                    System.out.println("Error: Division by zero!");
+                }
+                break;
+
+            default:
+                System.out.println("Invalid operator!");
+        }
+
+        sc.close();
+
+    }
+}
